ae8607b2e457e4e3299d18c1c6c1aae618f3fa84
Natti94 <natnael.berhane.sv@gmail.com>
2025-09-04 10:17:23 +0200

    added logic to Board.jsx

---
+++ src/game/Board.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+function Board() {
+  const [board, setBoard] = useState(Array(15).fill(Array(15).fill(null)));
+
+  async function handleBoardClick(rowIndex, colIndex) {
+    setBoard((prevBoard) => {
+      const newBoard = prevBoard.map((row) => [...row]);
+      newBoard[rowIndex][colIndex] = "X";
+      return newBoard;
+    });
+  }
+
+  return (
+    <div>
+      {board.map((row, rowIndex) => (
+        <div key={rowIndex} className="board-row">
+          {row.map((cell, colIndex) => (
+            <div
+              key={colIndex}
+              className="board-cell"
+              onClick={() => handleBoardClick(rowIndex, colIndex)}
+            >
+              {cell}
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Board;
