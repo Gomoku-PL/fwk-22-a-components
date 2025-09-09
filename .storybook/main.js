21a7799018c62aa265b8394556c8a1aa899f5c5d
msharif123 <mohamed.sharif.abdiwahab@gmail.com>
2025-09-09 16:07:25 +0200

    update

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,12 +3,14 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
   "stories": [
-    "../stories/**/*.mdx",
-    "../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    "../src/**/*.mdx",
     "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
   ],
   "addons": [
-    "@storybook/addon-docs"
+    "@chromatic-com/storybook",
+    "@storybook/addon-docs",
+    "@storybook/addon-a11y",
+    "@storybook/addon-vitest"
   ],
   "framework": {
     "name": "@storybook/react-vite",
