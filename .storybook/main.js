f95224350395771ac3f28e0580f7096def39511f
Melody Cartwright <melodycartwright@hotmail.com>
2025-09-11 11:52:26 +0200

    fix(main.js-storybook) accept incoming change

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,21 +1,4 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
-<<<<<<< HEAD
-const config = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
-    "@chromatic-com/storybook",
-    "@storybook/addon-docs",
-    "@storybook/addon-a11y",
-    "@storybook/addon-vitest"
-  ],
-  "framework": {
-    "name": "@storybook/react-vite",
-    "options": {}
-  }
-=======
 export default {
   // Only load *our* stories from src (we can keep the example folder in the repo,
   // it just won't be scanned by Storybook)
@@ -27,5 +10,4 @@ export default {
   framework: { name: "@storybook/react-vite", options: {} },
 
   docs: { autodocs: true },
->>>>>>> dd500617c7ba8ce620460d7a3b463ec2028e654e
 };
