9a1bf0045c9136f9b3cac450ca60968e82197377
Lara Govand <laaara@live.se>
2025-09-24 14:31:18 +0200

    feat: exportera komponenter via index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+export { default as Button } from './src/ui/Button/Button.jsx';
+export { default as Card } from './src/ui/Card.jsx';
+export { default as Modal } from './src/ui/Modal.jsx';
+export { default as Cell } from './src/game/Cell.jsx';
 
-export { default as Button } from './ui/Button/Button';
-export { Board } from './ui/Board/Board';
