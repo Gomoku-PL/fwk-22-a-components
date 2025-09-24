5c1287ef0efa3ed0c2f85d71ec4911993fb4b6ad
Lara Govand <laaara@live.se>
2025-09-24 15:01:14 +0200

    Remove unused Modal export

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 export { default as Button } from './src/ui/Button/Button.jsx';
 export { default as Card } from './src/ui/Card.jsx';
-export { default as Modal } from './src/ui/Modal.jsx';
 export { default as Cell } from './src/game/Cell.jsx';
 
