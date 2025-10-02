0a740d8e844bdd8e07340f64178b7b53b73aa345
Melody Cartwright <melodycartwright@hotmail.com>
2025-10-02 11:36:03 +0200

    REmoved extra barrel file

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-export { default as Board } from './src/game/Board.jsx';
-export { default as StatusBar } from './src/game/StatusBar.jsx';
-export { default as Button } from './src/ui/Button/Button.jsx';
-export { default as Card } from './src/ui/Card.jsx';
-export { default as Cell } from './src/game/Cell.jsx';
-
+// index.js (at package ROOT, next to package.json)
+export { default as Button } from "./src/ui/Button/Button.jsx";
+export { default as Board } from "./src/game/Board.jsx";
+export { default as StatusBar } from "./src/game/StatusBar.jsx";
+export { default as BoardWrapper } from "./src/ui/Board/BoardWrapper.jsx";
+export { default as Sidebar } from "./src/ui/Sidebar/Sidebar.jsx";
--- src/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Button } from '../src/ui/Button/Button.jsx';
-export { default as Board } from '../src/game/Board.jsx';
-export { default as StatusBar } from '../src/game/StatusBar.jsx';
-export { default as BoardWrapper } from '../src/ui/Board/BoardWrapper.jsx';
-export { default as Sidebar } from '../src/ui/Sidebar/Sidebar.jsx';
