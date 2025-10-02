214372bd37b1781d7a92163a11aed96cfb3d683c
Melody Cartwright <melodycartwright@hotmail.com>
2025-10-02 09:51:00 +0200

    Fixed exports

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 export { default as Button } from './ui/Button/Button.jsx';
 export { default as Board } from './game/Board.jsx';
 export { default as StatusBar } from './game/StatusBar.jsx';
-export { default as Board } from './ui/Board/Board.jsx';
+export { default as BoardWrapper } from './ui/Board/BoardWrapper.jsx';
 export { default as StatusBar } from './game/StatusBar.jsx';
+export { default as Sidebar } from './ui/Sidebar/Sidebar.jsx';
