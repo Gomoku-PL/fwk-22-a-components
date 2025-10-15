e50f239b6e37a725c4802ad9895f35fb640f5c2c
Melody Cartwright <melodycartwright@hotmail.com>
2025-10-15 23:12:34 +0200

    Export new components in index

--- index.js
+++ index.js
@@ -9,3 +9,5 @@ export { default as Button } from "./src/ui/Button/Button.jsx";
 export { default as Sidebar } from "./src/ui/Sidebar/Sidebar.jsx";
 export { default as Card } from "./src/ui/Card.jsx";
 export { default as CookieBanner } from "./src/ui/CookieBanner/CookieBanner.jsx";
+export { default as GDPRSupport } from "./src/ui/GDPRSupport/GDPRSupport.jsx";
+export { default as RegisterForm } from "./src/ui/RegisterForm/RegisterForm.jsx";
